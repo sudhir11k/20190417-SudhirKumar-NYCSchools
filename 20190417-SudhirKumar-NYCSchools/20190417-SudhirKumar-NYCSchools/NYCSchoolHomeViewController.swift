59a9efe997613fc2705461c4e5dd7aca378dabb0
Kumar, Sudhir <sudhir1308.k@gmail.com>
2019-04-17 22:39:36 -0500

    Making Connection with UITable to Display School Data

--- 20190417-SudhirKumar-NYCSchools/20190417-SudhirKumar-NYCSchools/NYCSchoolHomeViewController.swift
+++ 20190417-SudhirKumar-NYCSchools/20190417-SudhirKumar-NYCSchools/NYCSchoolHomeViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class NYCSchoolHomeViewController: UIViewController {
 
+    @IBOutlet weak var schoolDirectoryTable: UITableView!
+    
+    var managerNYCSchool = NYCSchoolManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        managerNYCSchool.getSchoolDirectoryList { (dataSource, customError) in
+            guard customError == nil else{
+                print("\(customError?.localizedDescription)")
+                return
+            }
+            
+            guard let data_Source = dataSource else{
+                print("Could Not load Table")
+                return
+            }
+            
+            
+            DispatchQueue.main.async {
+                self.schoolDirectoryTable.dataSource = data_Source
+                self.schoolDirectoryTable.reloadData()
+            }
+        }
     }
-
-
 }
 
